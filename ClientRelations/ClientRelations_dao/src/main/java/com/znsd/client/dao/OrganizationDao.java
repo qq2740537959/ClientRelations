0621ec304e5f5b7ca17ec70ae42c82009e82b005
jlh <jlh@qq.com>
2018-11-10 10:46:28 +0800

    增加page类，分页传的当前页数跟一页显示条数两个值用page类传值

---
+++ ClientRelations/ClientRelations_dao/src/main/java/com/znsd/client/dao/OrganizationDao.java
@@ -0,0 +1,14 @@
+package com.znsd.client.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 
+ * @author Administrator
+ *	组织结构dao
+ */
+@Repository
+public interface OrganizationDao {
+	
+	
+}
