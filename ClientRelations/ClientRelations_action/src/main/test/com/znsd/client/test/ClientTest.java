25810e937058ffdbea9b3561d45cf044b3597b9b
lfg <lfh@qq.com>
2018-11-10 15:56:46 +0800

    分页插件实现

--- ClientRelations/ClientRelations_action/src/main/test/com/znsd/client/test/ClientTest.java
+++ ClientRelations/ClientRelations_action/src/main/test/com/znsd/client/test/ClientTest.java
@@ -1,8 +1,6 @@
 package com.znsd.client.test;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.znsd.client.bean.ClientResource;
+import com.znsd.client.page.Pages;
 import com.znsd.client.service.ClientService;
 import com.znsd.client.vo.ClientVo;
 
@@ -20,16 +19,14 @@ public class ClientTest {
 	@Autowired
 	ClientService service;
 	
-	@Test  
+	@Test
 	public void tests() {
-		System.out.println(service);
-		ClientResource client = new ClientResource();
-		client.setStaffId(1);
-		client.setClientName("");
-		client.setPhone("13245697852");
-		List<ClientVo> clientVo = service.selectClientInfo(client);
+		Pages page = new Pages();
+		page.setCurrentIndexPage(1);
+		page.setPageSize(5);
+		List<ClientVo> clientVo = service.selectClientInfoByPage(2,null,null,page);
 		for (ClientVo clientVo2 : clientVo) {
-			System.out.println("-=--"+clientVo2.getClientName()+"  "+clientVo2.getStaffName());
+			System.out.println(clientVo2.getClientName()+"  "+clientVo2.getStaffName());
 		}
 	}
 	
--- ClientRelations/ClientRelations_service/src/main/java/com/znsd/client/service/ClientService.java
+++ ClientRelations/ClientRelations_service/src/main/java/com/znsd/client/service/ClientService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import com.znsd.client.bean.ClientResource;
+import com.znsd.client.page.Pages;
 import com.znsd.client.vo.ClientVo;
 
 public interface ClientService {
-	public List<ClientVo> selectClientInfo(ClientResource client); 
+
+	public List<ClientVo> selectClientInfoByPage(int staffId,String clientName,String phone, Pages page); 
 }
