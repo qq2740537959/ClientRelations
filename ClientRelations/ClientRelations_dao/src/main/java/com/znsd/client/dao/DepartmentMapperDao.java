c8ddc84b919cd312325b606108091b6b1bd99cff
jlh <jlh@qq.com>
2018-11-12 15:26:45 +0800

    增加分支

--- ClientRelations/ClientRelations_dao/src/main/java/com/znsd/client/dao/DepartmentMapperDao.java
+++ ClientRelations/ClientRelations_dao/src/main/java/com/znsd/client/dao/DepartmentMapperDao.java
@@ -3,11 +3,8 @@ package com.znsd.client.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-import com.znsd.client.bean.Page;
 import com.znsd.client.page.Pages;
 import com.znsd.client.vo.DepartmentVo;
 
